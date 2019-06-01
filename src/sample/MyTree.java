bf276f3421af3a9a53fccd8d7d545633d3f732fb
rflorian2017 <roby_florian@yahoo.com>
2019-06-01 10:38:33 +0300

    - daily coding 28_05

--- src/sample/MyTree.java
+++ src/sample/MyTree.java
@@ -1,10 +1,44 @@
 package sample;
 
+class Main2 {
+    public static void main(String[] args) {
+        MyTree tree = new MyTree();
+        tree.add(3); // root : val = 4, left, right = null
+        System.out.println(tree.root.value);
+        System.out.println(tree.root.left);
+        tree.add(7);
+        System.out.println(tree.root.right);
+        System.out.println(tree.root.right.value);
+
+        tree.add(2);
+        System.out.println(tree.root.left);
+        System.out.println(tree.root.left.value);
+    }
+}
+
 public class MyTree {
 
-    Node head;
+    Node root;
 
+    public void add(int i) {
+        Node node = new Node(i);
+        // add a first element
+        if (root == null) {
+            root = node;
+        }
+        else {
+            add(root, node);
+        }
+    }
 
+    private void add(Node root, Node newNode) {
+        if(newNode.value > root.value) {
+            root.right = newNode;
+        }
+        else {
+            root.left = newNode;
+        }
+    }
 
     class Node {
         int value;
