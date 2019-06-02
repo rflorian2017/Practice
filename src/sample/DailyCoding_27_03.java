28d8b23767d64dfceffe7e54f673b0b1474cd5b0
rflorian2017 <roby_florian@yahoo.com>
2019-06-02 10:28:51 +0300

    - daily coding 29_05

---
+++ src/sample/DailyCoding_27_03.java
@@ -0,0 +1,29 @@
+package sample;
+
+public class DailyCoding_27_03 {
+    /*
+    Good morning! Here's your coding interview problem for today.
+
+This problem was asked by Twitter.
+
+Implement an autocomplete system.
+That is, given a query string s and a set of all possible query strings,
+return all strings in the set that have s as a prefix.
+
+For example, given the query string de and the set of strings [dog, deer, deal],
+return [deer, deal].
+
+Hint: Try preprocessing the dictionary into a more efficient data structure to speed up queries.
+     */
+    public static void main(String[] args) {
+        String[] strings = {"dog", "deer", "deal"};
+        String query = "de";
+
+        for (int i = 0; i < strings.length; i++) {
+            if(strings[i].startsWith(query)) {
+                System.out.println(strings[i]);
+            }
+        }
+
+    }
+}
